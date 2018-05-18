a1121edf006533be9b5d66810c1618f1881a1244
paco <paco.xu@daocloud.io>
2018-05-18 14:51:27 +0800

    test

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,2 +1,25 @@
+#!/bin/sh
+
+# 检查这是否是初始提交
+if git rev-parse --verify HEAD >/dev/null 2>&1
+then
+    echo "pre-commit: About to create a new commit..."
+    against=HEAD
+else
+    echo "pre-commit: About to create the first commit..."
+    against=4b825dc642cb6eb9a060e54bf8d69288fbee4904
+fi
+
+# 使用git diff-index来检查空白字符错误
+echo "pre-commit: Testing for whitespace errors..."
+if ! git diff-index --check --cached $against
+then
+    echo "pre-commit: Aborting commit due to whitespace errors"
+    exit 1
+else
+    echo "pre-commit: No whitespace errors :)"
+    exit 0
+fi
+
 echo "Success Precommit"
 docker build . -t test:test
